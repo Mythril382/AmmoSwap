a2056f55e2c6e7c2b9e4d7010eae3ccc33059f0c
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-06-19 16:55:28 +0800

    stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,8 @@
-var debug = false; //if true, ammo swap data is shown in console
-
-Events.on(ClientLoadEvent, () => {
-  var it = Vars.content.blocks().select(b => b instanceof ItemTurret);
-  var lt = Vars.content.blocks().select(b => b instanceof LiquidTurret || b instanceof ContinuousLiquidTurret);
-  var itAmmo = new Seq();
-  var ltAmmo = new Seq();
+function randomizeAmmo() {
+  const it = Vars.content.blocks().select(b => b instanceof ItemTurret);
+  const lt = Vars.content.blocks().select(b => b instanceof LiquidTurret || b instanceof ContinuousLiquidTurret);
+  const itAmmo = new Seq();
+  const ltAmmo = new Seq();
   it.each(t => {
     itAmmo.add(t.ammoTypes.copy());
   });
@@ -16,14 +14,25 @@ Events.on(ClientLoadEvent, () => {
     if (b instanceof ItemTurret) {
       const t = itAmmo.random();
       itAmmo.remove(t);
-      if (debug) print("(" + b.name + "-" + b.ammoTypes + " -> " + t + ")");
+      if (Core.settings.getBool("ammo-swap-show-debug", false)) print(Core.bundle.format("ammo-swap-debug", t, b.localizedName));
       b.ammoTypes = t;
     }
     if (b instanceof LiquidTurret || b instanceof ContinuousLiquidTurret) {
       const t = ltAmmo.random();
       ltAmmo.remove(t);
-      if (debug) print("(" + b.name + "-" + b.ammoTypes + " -> " + t + ")");
+      if (Core.settings.getBool("ammo-swap-show-debug", false)) print(Core.bundle.format("ammo-swap-debug", t, b.localizedName));
       b.ammoTypes = t;
     }
   });
+}
+
+Events.on(ClientLoadEvent, () => {
+  Vars.ui.settings.addCategory("@setting.ammo-swap", Icon.turret, c => {
+    c.checkPref("ammo-swap-world-load", false);
+    c.checkPref("ammo-swap-show-debug", false);
+  });
+  
+  if (Core.settings.getBool("ammo-swap-world-load", false)) Events.on(WorldLoadEvent, () => randomizeAmmo()); 
+  
+  randomizeAmmo();
 });
